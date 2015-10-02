6d3ccfe37d94da1418eda4cca32a890bf0e8d1e5
RamaRivera <ramarivera@gmail.com>
2015-10-02 11:10:13 -0300

    EJ01 Completo

---
+++ EJ01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace EJ01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Animal[] iAnimales = new Animal[2];
+			Veterinaria iVeterinaria = new Veterinaria();
+
+			Gato iGato = new Gato();
+			Perro iPerro = new Perro();
+			
+
+			iAnimales[0] = iGato;
+			iAnimales[1] = iPerro;
+
+			iVeterinaria.AceptarAnimales(iAnimales);
+			Console.ReadKey();
+
+	
+		}
+	}
+}
---
+++ EJ01/Veterinaria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ01
+{
+    /// <summary>
+    /// Representa una veterinaria para la atencion de animales
+    /// </summary>
+	class Veterinaria
+	{
+        /// <summary>
+        /// Acepta los animales en la veterinaria, haciendo que emitan su sonido
+        /// </summary>
+        /// <param name="pAnimales">Arreglo que contiene objetos de la clase Animal</param>
+		public void AceptarAnimales(Animal[] pAnimales)
+		{
+			foreach (Animal iAnimal in pAnimales)
+			{
+				iAnimal.HacerRuido();
+			}
+		}
+	}
+}
