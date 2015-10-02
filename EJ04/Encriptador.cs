cce3c36d6eaa6d4d98a48537c556f223d3853d07
RamaRivera <ramarivera@gmail.com>
2015-10-02 11:11:16 -0300

    EJ04
    Estructura de Clases

---
+++ EJ04/Encriptador.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ04
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    abstract class Encriptador: IEncriptador
+    {
+        /// <summary>
+        /// Representa el Nombre del Encriptador
+        /// </summary>
+        private string iNombre;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pNombre"></param>
+        public Encriptador(string pNombre);
+
+        /// <summary>
+        /// Propiedad Nombre, solo lectura
+        /// </summary>
+        public string Nombre
+        {
+            get { return this.iNombre; }
+            private set { this.iNombre = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pCadena"></param>
+        /// <returns></returns>
+        public abstract string IEncriptador.Encriptar(string pCadena);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pCadena"></param>
+        /// <returns></returns>
+        public abstract string IEncriptador.Desencriptar(string pCadena);
+    }
+}
---
+++ EJ04/EncriptadorCesar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ04
+{
+    class EncriptadorCesar: Encriptador
+    {
+        private int iDesplazamiento;
+
+        public EncriptadorCesar(int pDesplazamiento)
+        {
+            this.iDesplazamiento = pDesplazamiento;
+        }
+
+        public override string Encriptar(string pCadena)
+        {
+            for (int i = 0; i < pCadena.Length; i++)
+            {
+                int aux = (Convert.ToInt32(pCadena[i])) + this.iDesplazamiento;
+                char aux1 = (Convert.ToChar(aux));
+                string nuevaCadena.Concat;
+            }
+        }
+
+    }
+}
