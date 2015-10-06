45a7e81763c96cf1c11ba926b3148bd309e0d57b
Ramiro Rivera <ramarivera@gmail.com>
2015-10-05 23:49:56 -0300

    EJ05 - Testing Unitario, Incompleto

---
+++ EJ05.Test/EncriptadorEnigmaTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EJ05;
+
+namespace EJ05.Test
+{
+    [TestClass]
+    public class EncriptadorEnigmaTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ EJ05.Test/FabricaEncriptadoresTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EJ05;
+using EJ04;
+
+namespace EJ05.Test
+{
+    [TestClass]
+    public class FabricaEncriptadoresTest
+    {
+        [TestMethod]
+        public void GetEncriptador_WithAlfabetico_All_ReturnsTrue()
+        {
+            string lCadena = "Ramiro Rivera";
+            string lResultado;
+            string lResultadoEsperado = lCadena;
+
+            EJ05.FabricaEncriptadores lFabrida = EJ05.FabricaEncriptadores.Instancia;
+
+            IEncriptador lEncriptadorNulo = lFabrida.GetEncriptador("NULO");
+            IEncriptador lEncriptadorCesar = lFabrida.GetEncriptador("CESAR");
+            IEncriptador lEncriptadorAES = lFabrida.GetEncriptador("AES");
+            IEncriptador lEncriptadorEnigma = lFabrida.GetEncriptador("ENIGMA");
+
+            lResultado = lEncriptadorCesar.Encriptar(lCadena);
+            lResultado = lEncriptadorEnigma.Encriptar(lResultado);
+            lResultado = lEncriptadorAES.Encriptar(lResultado);
+            lResultado = lEncriptadorNulo.Encriptar(lResultado);
+
+            lResultado = lEncriptadorNulo.Desencriptar(lResultado);
+            lResultado = lEncriptadorAES.Desencriptar(lResultado);
+            lResultado = lEncriptadorEnigma.Desencriptar(lResultado);
+            lResultado = lEncriptadorCesar.Desencriptar(lResultado);
+
+
+            Assert.AreEqual(lResultadoEsperado, lResultado);
+
+        }
+    }
+}
